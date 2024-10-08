43bfdb612ea5bcc3edd427c3e2be837d51f3686b
Ruka-s <rurukaneko2525@gmail.com>
2024-10-08 18:28:51 +0900

    写真日記保存アプリのプロトタイプ作成

---
+++ funcs.php
@@ -0,0 +1,52 @@
+<?php
+//共通に使う関数を記述
+
+//XSS対応（ echoする場所で使用！それ以外はNG ）
+function h($str)
+{
+    return htmlspecialchars($str, ENT_QUOTES);
+}
+
+
+//DBConnection
+function db_conn(){
+    try {
+        //Password:MAMP='root',XAMPP=''
+        // $pdo = new PDO('mysql:dbname=gs_book_db;charset=utf8;host=localhost','root','');
+        $pdo = new PDO('mysql:dbname=blueweasel38_unit1;charset=utf8;host=mysql57.blueweasel38.sakura.ne.jp','blueweasel38','unit1-unit1');
+        return $pdo;
+      } catch (PDOException $e) {
+        exit('DBError:'.$e->getMessage());
+      }
+    
+}
+
+
+//SQLエラー関数：sql_error($stmt)
+function sql_error($stmt){
+    $error = $stmt->errorInfo();
+    exit("SQLError:".$error[2]);
+}
+
+
+
+//リダイレクト関数: redirect($file_name)
+function redirect($filename){
+    header("Location: ".$filename);
+    exit();
+}
+//SessionCheck(スケルトン)
+function sschk(){
+    if(!isset($_SESSION["chk_ssid"]) || $_SESSION["chk_ssid"]!=session_id()){
+      exit("Login Error");
+    }else{
+      session_regenerate_id(true);
+      $_SESSION["chk_ssid"] = session_id();
+    }
+  }
+
+
+
+
+
+?>
